de3485896ed12aec1468ab514b718f892c0d8404
Nauzet Plasencia <nauzet.plasencia.cruz@gmail.com>
2022-01-16 15:30:16 +0100

    Add needed getters

--- src/Entity/EventType.php
+++ src/Entity/EventType.php
@@ -9,4 +9,19 @@ final class EventType
         private ?string $name,
         private ?string $code
     ) {}
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
 }
